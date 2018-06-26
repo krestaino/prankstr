c671f2cb55924136eaabbb51c2ee88c85425a615
Kevin Restaino <kevinrestaino@gmail.com>
2018-06-25 22:28:26 -0500

    fix: pause

--- services/TwiML.js
+++ services/TwiML.js
@@ -5,10 +5,12 @@ const TwiML = {
   build (hash) {
     return new Promise((resolve, reject) => {
       const xml = builder.create('Response')
-        .ele('Pause', {'type': 'length'}, '1')
+        .ele('Pause', {'length': '1'})
+        .up()
         .ele('Play', `https://prankstr.kmr.io/mp3/${hash}.mp3`)
-        .ele('Pause', {'type': 'length'}, '1')
-        .end({ pretty: true})
+        .up()
+        .ele('Pause', {'length': '1'})
+        .end({ allowEmpty: true, pretty: true})
         
       fs.writeFile('xml/' + hash + '.xml', xml, err => {
         resolve()
