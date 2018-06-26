d6dbd4066b1720fec6491f19b1a805e3e6de546f
Kevin Restaino <kevinrestaino@gmail.com>
2018-06-25 22:16:10 -0500

    feat: pause for 1 second before and after saying message

--- services/TwiML.js
+++ services/TwiML.js
@@ -5,7 +5,9 @@ const TwiML = {
   build (hash) {
     return new Promise((resolve, reject) => {
       const xml = builder.create('Response')
+        .ele('Pause', {'type': 'length'}, '1')
         .ele('Play', `https://prankstr.kmr.io/mp3/${hash}.mp3`)
+        .ele('Pause', {'type': 'length'}, '1')
         .end({ pretty: true})
         
       fs.writeFile('xml/' + hash + '.xml', xml, err => {
